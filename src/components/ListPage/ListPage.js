f5d942b88baec4d26f6e73002e526117aa46244d
Mengqi Wang <wmq516@gmail.com>
2019-06-13 16:27:53 -0600

    finished filter layout

--- src/components/ListPage/ListPage.js
+++ src/components/ListPage/ListPage.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
-function HomePage(props) {
+function ListPage(props) {
     return (
         <div>
             <h1>What's available in your neighborhood</h1>
 
+            <form>
+                <input type="radio" name="listing-type" value="0" />
+                <label for="listing-type">
+                    <span>Vegetable</span>
+                </label>
 
+                <input type="radio" name="listing-type" value="1" />
+                <label for="listing-type">
+                    <span>Fruit</span>
+                </label>
+            </form>
+
+            <ul>
+                <li>5 organic tomatoes</li>
+                <li>fresh peaches</li>
+                <li>watermelon seeds</li>
+                <li>5lbs potatoes</li>
+            </ul>
         </div>
     );
 };
 
-export default HomePage;
+export default ListPage;
