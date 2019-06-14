fd5fd5aff7426ee25c4fef0f8425146555e8188d
Mengqi Wang <wmq516@gmail.com>
2019-06-14 12:43:59 -0600

    added localStorage and programmed Register button

--- src/components/RegistrationPage/RegistrationPage.js
+++ src/components/RegistrationPage/RegistrationPage.js
@@ -2,10 +2,16 @@ import React from 'react'
 import RegistrationForm from '../RegistrationForm/RegistrationForm'
 
 function RegistrationPage(props) {
+
+    function handleRegistrationSuccess() {
+        const { history } = props
+        history.push('/login')
+    }
+
     return (
         <div className='RegistrationPage'>
             <h2>Register</h2>
-            <RegistrationForm />
+            <RegistrationForm onRegistrationSuccess={handleRegistrationSuccess} />
         </div>
     )
 }
