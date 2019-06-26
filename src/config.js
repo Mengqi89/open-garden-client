9d6c16e57c1f9ced734ecb4c7e4fec9d01e8ca7b
Mengqi Wang <wmq516@gmail.com>
2019-06-26 00:47:29 -0600

    changed API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://enigmatic-shelf-57504.herokuapp.com/api',
     TOKEN_KEY: 'food-share-client-auth-token'
 }
