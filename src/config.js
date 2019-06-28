df83c9aba512ee22bb814abaab0e2c96f4ea4838
Mengqi Wang <wmq516@gmail.com>
2019-06-28 11:31:45 -0600

    fixed API_ENDPOINT typo and compressed hero image

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://enigmatic-shelf-57504.herokuapp.com//api',
+    API_ENDPOINT: 'https://enigmatic-shelf-57504.herokuapp.com/api',
     TOKEN_KEY: 'food-share-client-auth-token'
 }
