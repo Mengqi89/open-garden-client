c954276294b1f4fdee2833c937326c414cd2ffe5
Mengqi Wang <wmq516@gmail.com>
2020-05-28 18:26:07 -0600

    added BrowserRouter to two tests with Link

--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import LoginForm from './LoginForm'
+import { BrowserRouter } from 'react-router-dom'
 
 it('renders without crashing', () => {
     const div = document.createElement('div')
 
     ReactDom.render(
-        <LoginForm />, div)
+        <BrowserRouter><LoginForm /></BrowserRouter>, div)
 
     ReactDom.unmountComponentAtNode(div)
 
--- src/components/RegistrationForm/RegistrationForm.test.js
+++ src/components/RegistrationForm/RegistrationForm.test.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import RegistrationForm from './RegistrationForm'
+import { BrowserRouter } from 'react-router-dom'
 
 it('renders without crashing', () => {
 
     const div = document.createElement('div')
 
     ReactDom.render(
-        <RegistrationForm />
-        , div)
+        <BrowserRouter>
+            <RegistrationForm />
+        </BrowserRouter>, div)
 
     ReactDom.unmountComponentAtNode(div)
 
