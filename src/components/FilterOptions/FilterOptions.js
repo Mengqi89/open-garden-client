446aa9dc4975638a655844b293d5ceb052dee1f8
Mengqi Wang <wmq516@gmail.com>
2020-06-09 11:26:59 -0600

    styled radio buttons for FilterOptions

--- src/components/FilterOptions/FilterOptions.js
+++ src/components/FilterOptions/FilterOptions.js
@@ -5,7 +5,6 @@ function FilterOptions(props) {
     return (
         <form className='FilterOptions'>
             <fieldset>
-                <legend>Only see what you like:</legend>
                 <input type='radio' name='listing-type' id='vegetable' value='vegetable' onChange={event => props.handleFilterChange(event.target.value)} />
                 <label htmlFor='vegetable'>
                     <span>Vegetable</span>
